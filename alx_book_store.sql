831d205ff9389aa84ee129c88835c56f409e6a7f
Ominde Samson <sonrollwriters@gmail.com>
2025-08-18 00:20:30 +0300

    Changed The Cases

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,13 +5,13 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 -- STEP 3: CREATE AUTHORS TABLE
-CREATE TABLE authors (
+CREATE TABLE Authors (
   author_id INT AUTO_INCREMENT PRIMARY KEY,
   author_name VARCHAR(215) NOT NULL
 );
 
 -- STEP 4: CREATE BOOKS TABLE
-CREATE TABLE books (
+CREATE TABLE Books (
   book_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(130) NOT NULL,
   author_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE books (
 );
 
 -- STEP 5: CREATE CUSTOMERS TABLE
-CREATE TABLE customers (
+CREATE TABLE Customers (
   customer_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_name VARCHAR(215) NOT NULL,
   email VARCHAR(215),
@@ -29,7 +29,7 @@ CREATE TABLE customers (
 );
 
 -- STEP 6: CREATE ORDERS TABLE
-CREATE TABLE orders (
+CREATE TABLE Orders (
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_id INT,
   order_date DATE,
@@ -37,7 +37,7 @@ CREATE TABLE orders (
 );
 
 -- STEP 7: CREATE ORDER_DETAILS TABLE
-CREATE TABLE order_details (
+CREATE TABLE Order_details (
   orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
   order_id INT,
   book_id INT,
